b881431984fe2aacc2a2f5c47d4be645ae61ef6b
Davide Scuderi <scuderidavide@libero.it>
2026-08-30 03:01:50 +0200

    Anteprima riservata dei richiami per lo studio (v38)

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ try {
   importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 } catch (e) {}
 
-var CACHE = 'ks-v37';
+var CACHE = 'ks-v38';
 var ASSETS = [
   './',
   './index.html',
