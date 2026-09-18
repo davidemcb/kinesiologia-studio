a6aee66d15bad0df07f659318a962ee6680846bc
Davide Scuderi <scuderidavide@libero.it>
2026-09-18 23:01:38 +0200

    Anatomia: schemi a tutta larghezza e link condiviso che resta sulla voce (v45)

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ try {
   importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 } catch (e) {}
 
-var CACHE = 'ks-v44';
+var CACHE = 'ks-v45';
 var ASSETS = [
   './',
   './index.html',
