c6a2bfb55f760eb8126fad7fb055f40e7a1fbffd
Davide Scuderi <scuderidavide@libero.it>
2026-09-19 00:43:15 +0200

    Posizionamento: lavoro perche' tu non abbia piu' bisogno di me; le righe sugli altri riscritte in positivo (v49)

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ try {
   importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 } catch (e) {}
 
-var CACHE = 'ks-v48';
+var CACHE = 'ks-v49';
 var ASSETS = [
   './',
   './index.html',
